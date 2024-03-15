11cd719c13fbd5112fbdb5301c2857854b03c89a
Marek Skrobacki <skrobul@skrobul.com>
2024-03-15 10:30:09 +0000

    fix mappings for base64

--- lua/plugins/base64.lua
+++ lua/plugins/base64.lua
@@ -4,8 +4,8 @@ return {
   keys = {
     -- Decode/encode selected sequence from/to base64
     -- (mnemonic: code[b]ase64)
-    { "<Leader>cb", "<Plug>(FromBase64)", mode = "x" },
-    { "<Leader>cB", "<Plug>(ToBase64)", mode = "x" },
+    { "<Leader>cb", "<Plug>(FromBase64)", mode = "x", desc = "Decode B64" },
+    { "<Leader>cB", "<Plug>(ToBase64)", mode = "x", desc = "Encode B64" },
   },
   config = function()
     require("nvim-base64").setup()
