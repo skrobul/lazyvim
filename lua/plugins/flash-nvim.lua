d26a6bf9300ad12ad20fc06385ea5168ef2aa050
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-09-07 12:35:47 +0100

    disable flash-nvim

--- lua/plugins/flash-nvim.lua
+++ lua/plugins/flash-nvim.lua
@@ -1,15 +1,19 @@
 return {
-  { 
+  {
     "folke/flash.nvim",
     opts = {
       modes = {
         search = {
-          enabled = false
+          enabled = false,
         },
         char = {
-          enabled = false
-        }
-      }
-    }
-  }
+          enabled = false,
+        },
+      },
+    },
+    keys = {
+      -- disable the default flash keymap as it breaks the surround
+      -- { "s", mode = { "n", "x", "o" }, false },
+    },
+  },
 }
