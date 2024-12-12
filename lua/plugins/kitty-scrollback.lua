95bdc207d7bca7133162a9b365b68dfd6fe0184c
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-12-12 12:57:18 +0000

    fix kitty-scrollback lua

--- lua/plugins/kitty-scrollback.lua
+++ lua/plugins/kitty-scrollback.lua
@@ -4,8 +4,8 @@ return {
   lazy = true,
   cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
   event = { "User KittyScrollbackLaunch" },
-  -- version = '*', -- latest stable version, may have breaking changes if major version changed
-  version = "^4.2.1", -- pin major version, include fixes and features that do not have breaking changes
+  version = '*', -- latest stable version, may have breaking changes if major version changed
+  -- version = "^4.2.1", -- pin major version, include fixes and features that do not have breaking changes
   config = function()
     require("kitty-scrollback").setup()
   end,
