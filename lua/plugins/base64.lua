09f6285d212029d940a7ae0c6993792134b3b6af
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-12-12 12:57:59 +0000

    base64: better mappings

--- lua/plugins/base64.lua
+++ lua/plugins/base64.lua
@@ -4,8 +4,8 @@ return {
   keys = {
     -- Decode/encode selected sequence from/to base64
     -- (mnemonic: code[b]ase64)
-    { "<Leader>cb", "<Plug>(FromBase64)", mode = "x", desc = "Decode B64" },
-    { "<Leader>cB", "<Plug>(ToBase64)", mode = "x", desc = "Encode B64" },
+    { "<Leader>cbd", "<Plug>(FromBase64)", mode = "x", desc = "Decode B64" },
+    { "<Leader>cbe", "<Plug>(ToBase64)", mode = "x", desc = "Encode B64" },
   },
   config = function()
     require("nvim-base64").setup()
