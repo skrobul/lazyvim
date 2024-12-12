2656c4b899e3ad9a0588d874a6cce5db263e2693
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-12-12 12:56:35 +0000

    oil: watch for changes

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,9 @@
 return {
   {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+        watch_for_changes = true,
+    },
   cmd = "Oil",
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
