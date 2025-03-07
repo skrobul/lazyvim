9430a1de7170a33efb99a98abc4a14dac2a29f76
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2025-03-07 18:13:36 +0000

    switch from nvim-base64 to endec

--- lua/plugins/base64.lua
+++ lua/plugins/base64.lua
@@ -1,13 +1,74 @@
 return {
-  "deponian/nvim-base64",
+  "ovk/endec.nvim",
   version = "*",
-  keys = {
-    -- Decode/encode selected sequence from/to base64
-    -- (mnemonic: code[b]ase64)
-    { "<Leader>cbd", "<Plug>(FromBase64)", mode = "x", desc = "Decode B64" },
-    { "<Leader>cbe", "<Plug>(ToBase64)", mode = "x", desc = "Encode B64" },
+  event = "VeryLazy",
+  opts = {
+    -- Key mappings.
+    -- Mapping 	Description
+    -- Defaults:
+    -- gb 	Decode Base64 in a popup
+    -- gyb 	Decode Base64 in-place
+    -- gB 	Encode Base64 in-place
+    -- gs 	Decode Base64URL in a popup
+    -- gys 	Decode Base64URL in-place
+    -- gS 	Encode Base64URL in-place
+    -- gl 	Decode URL in a popup
+    -- gyl 	Decode URL in-place
+    -- gL 	Encode URL in-place
+    -- keymaps = {
+    --   -- Decode Base64 in-place (normal mode)
+    --   decode_base64_inplace = "<Leader>cbd",
+    --
+    --   -- Decode Base64 in-place (visual mode)
+    --   vdecode_base64_inplace = "<Leader>cbd",
+    --
+    --   -- Decode Base64 in a popup (normal mode)
+    --   decode_base64_popup = "<Leader>cbD",
+    --
+    --   -- Decode Base64 in a popup (visual mode)
+    --   vdecode_base64_popup = "<Leader>cbd",
+    --
+    --   -- Encode Base64 in-place (normal mode)
+    --   encode_base64_inplace = "<Leader>cbe",
+    --
+    --   -- Encode Base64 in-place (visual mode)
+    --   vencode_base64_inplace = "<Leader>cbe",
+    --
+    --   -- Decode Base64URL in-place (normal mode)
+    --   decode_base64url_inplace = "<Leader>cbud",
+    --
+    --   -- Decode Base64URL in-place (visual mode)
+    --   vdecode_base64url_inplace = "<Leader>cbud",
+    --
+    --   -- Decode Base64URL in a popup (normal mode)
+    --   decode_base64url_popup = "<Leader>cbup",
+    --
+    --   -- Decode Base64URL in a popup (visual mode)
+    --   vdecode_base64url_popup = "<Leader>cbup",
+    --
+    --   -- Encode Base64URL in-place (normal mode)
+    --   encode_base64url_inplace = "<leader>cbue",
+    --
+    --   -- Encode Base64URL in-place (visual mode)
+    --   vencode_base64url_inplace = "<leader>cbue",
+    --
+    --   -- Decode URL in-place (normal mode)
+    --   decode_url_inplace = "<leader>cud",
+    --
+    --   -- Decode URL in-place (visual mode)
+    --   vdecode_url_inplace = "<leader>cud",
+    --
+    --   -- Decode URL in a popup (normal mode)
+    --   decode_url_popup = "<leader>cud",
+    --
+    --   -- Decode URL in a popup (visual mode)
+    --   vdecode_url_popup = "<leader>cup",
+    --
+    --   -- Encode URL in-place (normal mode)
+    --   encode_url_inplace = "<leader>cue",
+    --
+    --   -- Encode URL in-place (visual mode)
+    --   vencode_url_inplace = "<leader>cue",
+    -- },
   },
-  config = function()
-    require("nvim-base64").setup()
-  end,
 }
