21ae7b70f7b95a7dca5a9f73cc0347d909223470
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2025-08-04 11:05:06 +0100

    vim coilorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,20 @@
+return {
+  -- Noirbuddy
+  {
+    "jesseleite/nvim-noirbuddy",
+    dependencies = {
+      { "tjdevries/colorbuddy.nvim" },
+    },
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- All of your `setup(opts)` will go here
+    },
+  },
+  -- nightfox
+  { "EdenEast/nightfox.nvim" },
+  { "datsfilipe/vesper.nvim" },
+  { "kvrohit/substrata.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "webhooked/kanso.nvim" }
+}
